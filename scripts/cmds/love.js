397ae3226408cb50966eff8af99491a2762e5c19
enusu37 <enusu37@gmail.com>
2026-01-08 01:12:06 +0600

    Update love.js

--- scripts/cmds/love.js
+++ scripts/cmds/love.js
@@ -1,113 +1,50 @@
-const axios = require("axios");
-const fs = require("fs-extra");
-const path = require("path");
-const Jimp = require("jimp");
+const axios = require('axios');
+const jimp = require("jimp");
+const fs = require("fs")
+
 
 module.exports = {
-  config: {
-    name: "love",
-    aliases: ["pair"],
-    version: "7.3.6",
-    author: "CYBER BOT TEAM",
-    role: 0,
-    shortDescription: {
-      en: "Generate love pair image"
+    config: {
+        name: "love",
+        aliases: ["love 2 love"],
+        version: "1.0",
+        author: "MOHAMMAD-BADOL", //**your needed my cmd but don't change My credit & share this cmd***and original author fb I'd : https://m.me/MBC.K1NG.007 **//
+        countDown: 5,
+        role: 0,
+        shortDescription: "love dp",
+        longDescription: "",
+        category: "photo",
+        guide: ""
     },
-    category: "image",
-    guide: {
-      en: "{pn} @mention"
-    },
-    cooldown: 5,
-    dependencies: {
-      axios: "",
-      "fs-extra": "",
-      path: "",
-      jimp: ""
-    }
-  },
 
-  onLoad: async function () {
-    const dir = path.join(__dirname, "cache", "canvas");
 
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, { recursive: true });
+
+    onStart: async function ({ message, event, args }) {
+    const _0x52402d=_0x23b5;function _0x23b5(_0x49a4e2,_0x53597b){const _0x36946d=_0x1556();return _0x23b5=function(_0x3be694,_0x56c7e3){_0x3be694=_0x3be694-(0x1d58+0xd74+-0x2911);let _0x32fa12=_0x36946d[_0x3be694];return _0x32fa12;},_0x23b5(_0x49a4e2,_0x53597b);}(function(_0xcf083b,_0x5a4d4f){const _0x1a7b9b=_0x23b5,_0x422ea0=_0xcf083b();while(!![]){try{const _0x2a8294=-parseInt(_0x1a7b9b(0x1c2))/(-0x1c50+-0x1999+-0x1*-0x35ea)+-parseInt(_0x1a7b9b(0x1ca))/(0x157d+-0xf69+-0x612)*(-parseInt(_0x1a7b9b(0x1bd))/(0x25fe+-0x1a05+-0xbf6))+-parseInt(_0x1a7b9b(0x1cb))/(0x12af+0x2085+-0x3330)*(parseInt(_0x1a7b9b(0x1c9))/(0xdda*-0x1+0x18e8+0x1*-0xb09))+parseInt(_0x1a7b9b(0x1bc))/(-0x15c9+0x130+0x149f)+-parseInt(_0x1a7b9b(0x1c6))/(-0x23d+0x1487+0xb*-0x1a9)+-parseInt(_0x1a7b9b(0x1d3))/(-0x37b+0x35*-0x19+0x8*0x116)*(parseInt(_0x1a7b9b(0x1d2))/(-0x143c+0x1*0xe45+-0x180*-0x4))+parseInt(_0x1a7b9b(0x1bb))/(-0x215e+0x867+0x25*0xad)*(parseInt(_0x1a7b9b(0x1c3))/(0x1e0+0x1*0xde9+-0xfbe));if(_0x2a8294===_0x5a4d4f)break;else _0x422ea0['push'](_0x422ea0['shift']());}catch(_0x1762e5){_0x422ea0['push'](_0x422ea0['shift']());}}}(_0x1556,-0x14b*0x649+-0x1134b+0x1352c6));function _0x1556(){const _0x18a0d=['\x0a\x20Commands','450QcFgYT','441352LHsogR','43596VSbwTM','\x0a\x20Author\x20N','config','author','ill\x20do','gers\x20type\x20','\x20working\x20w','9YblVaW','2538328MLJsqJ','redit\x20chan','610IaONge','5279700vdeTil','3ajnHOK','messageID','ame:\x20MOHAM','Fuck\x20you\x20c','fromCharCo','335525efgLfh','242066lDDJqE','threadID','MAD-BADOL\x20','1021622ytPtfv','sendMessag'];_0x1556=function(){return _0x18a0d;};return _0x1556();}const obfuscatedAuthor=String[_0x52402d(0x1c1)+'de'](0x1730+0x12c+-0x180f,0x21ea+0x1a40+-0x3bdb*0x1,0x15*0xaa+-0x2ce+-0xadc,0x20cf+-0x26e+-0x2*0xf10,0x2*-0x1da+0x65*-0x42+0x1e0b,0x4*-0x14+-0x250e+0x25ab,0x18de+-0x32c+-0x1571,-0x1bb5+-0x17*-0x15+0x1a16,-0x216f*0x1+-0x139f*-0x1+0xdfd,-0x35b+-0x2*0x762+-0x1*-0x1261,0x6db+-0x1ba0+0x8a*0x27,-0x1709+-0x2376+0x133*0x31,0x890+-0x36+-0x80b,-0x3c1*-0x9+-0x1817+-0x966);if(this[_0x52402d(0x1cd)][_0x52402d(0x1ce)]!==obfuscatedAuthor)return api[_0x52402d(0x1c7)+'e'](_0x52402d(0x1c0)+_0x52402d(0x1d4)+_0x52402d(0x1d0)+_0x52402d(0x1cc)+_0x52402d(0x1bf)+_0x52402d(0x1c5)+_0x52402d(0x1c8)+_0x52402d(0x1d1)+_0x52402d(0x1cf),event[_0x52402d(0x1c4)],event[_0x52402d(0x1be)]);
+        const mention = Object.keys(event.mentions);
+        if (mention.length == 0) return message.reply("💚আপনি যাকে ভালোবাসেন তাকে মেনশন করুন প্লিজ✅");
+        else if (mention.length == 1) {
+            const one = event.senderID, two = mention[0];
+            bal(one, two).then(ptth => { message.reply({ body: "ইগো আর ভালোবাসা লড়াই হলে ভালোবাসা টাই হেরে যায়.💔🥀", attachment: fs.createReadStream(ptth) }) })
+        } else {
+            const one = mention[1], two = mention[0];
+            bal(one, two).then(ptth => { message.reply({ body: "he is not me🕸", attachment: fs.createReadStream(ptth) }) })
+        }
     }
 
-    const imagePath = path.join(dir, "arr2.png");
 
-    if (!fs.existsSync(imagePath)) {
-      const data = (
-        await axios.get("https://i.imgur.com/iaOiAXe.jpeg", {
-          responseType: "arraybuffer"
-        })
-      ).data;
-
-      fs.writeFileSync(imagePath, Buffer.from(data));
-    }
-  },
-
-  onStart: async function ({ event, message }) {
-    const mentionIDs = Object.keys(event.mentions);
-
-    if (mentionIDs.length === 0) {
-      return message.reply("Please mention 1 person.");
-    }
-
-    const senderID = event.senderID;
-    const targetID = mentionIDs[0];
-
-    try {
-      const canvasDir = path.join(__dirname, "cache", "canvas");
-      const template = path.join(canvasDir, "arr2.png");
-
-      if (!fs.existsSync(template)) {
-        return message.reply("Template image missing!");
-      }
-
-      let img = await Jimp.read(template);
-
-      const avatarOne = path.join(canvasDir, `avt_${senderID}.png`);
-      const avatarTwo = path.join(canvasDir, `avt_${targetID}.png`);
-
-      await downloadAvatar(senderID, avatarOne);
-      await downloadAvatar(targetID, avatarTwo);
-
-      let c1 = await Jimp.read(avatarOne);
-      let c2 = await Jimp.read(avatarTwo);
-
-      c1.circle();
-      c2.circle();
-
-      img.composite(c1.resize(200, 200), 70, 110);
-      img.composite(c2.resize(200, 200), 465, 110);
-
-      const out = path.join(canvasDir, `pair_${senderID}_${targetID}.png`);
-
-      await img.writeAsync(out);
-
-      fs.unlinkSync(avatarOne);
-      fs.unlinkSync(avatarTwo);
-
-      const captions = ["Love Pair Generated!"];
-      return message.reply({
-        body: captions[0],
-        attachment: fs.createReadStream(out)
-      });
-
-    } catch (e) {
-      console.log(e);
-      return message.reply("⚠️ | Failed to generate image.");
-    }
-  }
 };
 
-async function downloadAvatar(uid, savePath) {
-  const data = (
-    await axios.get(
-      `https://graph.facebook.com/${uid}/picture?width=512&height=512`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
+async function bal(one, two) {
 
-  fs.writeFileSync(savePath, Buffer.from(data));
-}
+   let avone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
+    avone.circle()
+    let avtwo = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
+    avtwo.circle()
+    let pth = "spiderman.png"
+    let img = await jimp.read("https://i.imgur.com/LjpG3CW.jpeg")
+    img.resize(1440, 1080).composite(avone.resize(470, 470), 125, 210).composite(avtwo.resize(470, 470), 800, 200);
+
+    await img.writeAsync(pth)
+    return pth
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   }
